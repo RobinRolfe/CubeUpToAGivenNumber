0986919853d7d5f6de593ac42701a8896be343be
ROBIN ALEXANDER ROLFE <S14001664@mail.glyndwr.ac.uk>
2018-07-27 19:47:36 +0100

    Add project files.

---
+++ CubeUpToAGivenNumber/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace CubeUpToAGivenNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Insert a number to cube");
+
+            int[] numarray = new int[Convert.ToInt32(Console.ReadLine())];
+            int value = 1;
+
+
+            for (int i = 0; i < numarray.Length; i++)
+            {
+                numarray.SetValue(value, i);
+                value++;
+            }
+
+            for (int i = 0; i < numarray.Length; i++)
+            {
+
+                Console.WriteLine("the number is " + numarray[i] + " and the cube of " + numarray[i] + " is " + numarray[i] * numarray[i] * numarray[i]);
+
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
